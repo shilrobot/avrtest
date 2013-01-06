dc6d56d58c49824dbceb41a1e9c5cc3f3ae6d920
Scott Hilbert <github@shilbert.com>
2013-01-05 19:34:07 -0500

    spaces

--- blink.c
+++ blink.c
@@ -13,16 +13,16 @@ char data[16160] PROGMEM;
 int main(void)
 {
 
-	// Blink pin 5 on port B
-	// (the LED on digital port 13 on Arduino)
+    // Blink pin 5 on port B
+    // (the LED on digital port 13 on Arduino)
 
-	DDRB = (1<<5);
+    DDRB = (1<<5);
 
-	while(1)
-	{
-		PORTB |= (1<<5);
-		_delay_ms(500);
-		PORTB &= ~(1<<5);
-		_delay_ms(250);
-	}
+    while(1)
+    {
+        PORTB |= (1<<5);
+        _delay_ms(500);
+        PORTB &= ~(1<<5);
+        _delay_ms(250);
+    }
 }
